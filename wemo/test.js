b0c271bceab2ce17fb8e3ee051f18258e5cf2944
Tom Mackall <mackall.tom@gmail.com>
2016-12-02 03:29:09 -0800

    Wemo cleanup.

--- wemo/test.js
+++ wemo/test.js
@@ -1,20 +1,43 @@
 'use strict';
-var Wemo = require('wemo-client');
+const winston = require('winston');
+const Wemo = require('wemo-client');
 var wemo = new Wemo();
+var logLevel = process.env.LL || 'info';
 
-wemo.discover(function(deviceInfo) {
-  console.log('Wemo Device Found: %j', deviceInfo);
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({'timestamp':true, level: logLevel})
+   ]
+});
 
-  // Get the client for the found device
-  var client = wemo.client(deviceInfo);
-
-  // Handle BinaryState events
-/*
-  client.on('binaryState', function(value) {
-    console.log('Binary State changed to: %s', value);
+function getClient(callback) {
+  wemo.discover(function(deviceInfo) {
+    logger.debug(JSON.stringify(deviceInfo, null, 2));
+    logger.info('Name: %j', deviceInfo.friendlyName);
+    logger.info('host: %j', deviceInfo.host);
+    logger.debug('state: %j', deviceInfo.binaryState);
+  
+    // Get the client for the found device
+    var client = wemo.client(deviceInfo);
+    callback(null, client);
   });
+}
+var client = null;
+var argv = require('optimist')
+  .usage('Turns lamp on and off.\nUsage: $0 <-o> (default is on)')
+  .boolean('o')
+  .argv
+;
+var off = argv.o;
+var state = 1;
 
-*/
-  // Turn the switch on
-  client.setBinaryState(1);
+getClient(function(err, res) {  
+  client = res;
+  if (off === true) {
+    state = 0;
+  }
+  client.setBinaryState(state, function(err) {
+    if (err) logger.error(err), process.exit(3);
+    process.exit(0);
+  });
 });
